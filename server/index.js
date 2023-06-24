908bfcaf3dff0133b791afc3f990023f214392c3
Shubham More <97827118+shubhamore@users.noreply.github.com>
2023-06-24 17:24:55 +0530

    removed the trailing slash in cors  from index.js

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ mongoose
   .catch((err) => console.log(err));
 
 app.use(express.json());
-app.use(cors({ credentials: true, origin: "https://blog-point-shubham.netlify.app/" }));
+app.use(cors({ credentials: true, origin: "https://blog-point-shubham.netlify.app" }));
 app.use(cookieParser());
 app.use("/uploads", express.static(__dirname + "/uploads"));
 
